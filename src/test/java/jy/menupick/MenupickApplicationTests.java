8814b3ee0224a428ab0ca0b64a9435579a505254
jiyeon0926 <yeoni9094@naver.com>
2025-02-24 00:00:06 +0900

    init

---
+++ src/test/java/jy/menupick/MenupickApplicationTests.java
@@ -0,0 +1,13 @@
+package jy.menupick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenupickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
